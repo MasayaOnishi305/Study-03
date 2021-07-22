369471ea575ecfc8da6fca82266dd14c403a6970
MasayaOnishi305 <72341237+MasayaOnishi305@users.noreply.github.com>
2021-07-22 19:12:40 +0900

    課題3完成

--- search.py
+++ search.py
@@ -2,22 +2,27 @@ import pandas as pd
 import eel
 
 ### デスクトップアプリ作成課題
-def kimetsu_search(word):
+def kimetsu_search(word,csv_file,add_flg):
     # 検索対象取得
-    df=pd.read_csv("./source.csv")
+    df=pd.read_csv(f"./{csv_file}.csv")
     source=list(df["name"])
 
     # 検索
     if word in source:
-        print("『{}』はあります".format(word))
+        answer = f"『{word}』はあります"
+    
     else:
-        print("『{}』はありません".format(word))
+        answer = f"『{word}』はありません"      
         # 追加
         #add_flg=input("追加登録しますか？(0:しない 1:する)　＞＞　")
-        #if add_flg=="1":
-        source.append(word)
-    
-    # CSV書き込み
-    df=pd.DataFrame(source,columns=["name"])
-    df.to_csv("./source.csv",encoding="utf_8-sig")
-    print(source)
+        if add_flg=="1":
+            source.append(word)
+            print(f"{source}.csvに{word}を追加登録します")
+            # CSV書き込み
+            df=pd.DataFrame(source,columns=["name"])
+            df.to_csv(f"./{csv_file}.csv",encoding="utf_8-sig")
+            answer = f"{answer}\r\n{source}"
+
+    print(answer)
+    return(answer)
+
